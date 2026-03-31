7b148e0a03828e59a2c541f8154414457b37f786
Lastmaky <juverjimenezp@gmail.com>
2026-03-30 23:58:15 -0300

    Configurar GitHub Actions para publicar la web

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+    site: 'https://lastmaky.github.io', 
+
+  base: '/nathalie-portfolio',
   integrations: [tailwind()],
 });
